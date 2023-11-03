178998016303c447d6c3e929d62686e63f4952f7
Gabriel Henrique <122419125+gabrielhcm@users.noreply.github.com>
2023-11-02 22:06:59 -0300

    Definição das opçoes e uso de pilhas
    fiz a definição das opçoes por enquanto, ta faltando fazer o retorno da pilhas para poder imprimi-las, ta feio pra caraleo mas é um começo.

--- Torre.py
+++ Torre.py
@@ -1,46 +1,46 @@
-escolha = ()
-
-peçassegura = ()
-
-while  escolha !=(int):
-
-    print( " Escolha uma das opçoes:\n 0 - retirar uma peça de lugar \n 1 - colocar uma peça em outro disco\n 2 - mostrar os tres discos\n 3 - mostrar o disco que você está segurando ")    
-
-    escolha = (input())
-    print(escolha)
-#while True:
-
-
-    if escolha == 0:
-        
-
-    elif escolha == 1:
-
-
-    elif escolha == 2:
-
-
-
-    elif escolha == 3:
-        
-        if peçassegura == ():
-            print("Você nao esta com nenhuma peça em mãos")
-
-
-
-
-
-class Noh:
+class Noh1:
     def __init__(self, valor):
         self.valor = valor
         self.proximo = None
 
-class PilhaEncadeada:
+class PilhaEncadeada1:
+    def __init__(self):
+        self.cabeca = None
+        self.size = 0
+    def isEmpty(self):
+        return self.size == 0
+ 
+    # Get the top item of the stack
+    def peek(self):
+        # Sanitary check to see if we
+        # are peeking an empty stack.
+        if self.isEmpty():
+            raise Exception("Peeking from an empty stack")
+        return self.cabeca.proximo.valor
+    def push(self, valor):
+        novo_no = Noh1(valor)
+        novo_no.proximo = self.cabeca
+        self.cabeca = novo_no
+        self.size += 1
+
+    def pop(self):
+        removido = self.cabeca.valor
+        self.cabeca = self.cabeca.proximo
+        self.size -= 1
+        return removido
+
+
+class Noh2:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+class PilhaEncadeada2:
     def __init__(self):
         self.cabeca = None
 
     def push(self, valor):
-        novo_no = Noh(valor)
+        novo_no = Noh2(valor)
         novo_no.proximo = self.cabeca
         self.cabeca = novo_no
 
@@ -48,23 +48,101 @@ class PilhaEncadeada:
         removido = self.cabeca.valor
         self.cabeca = self.cabeca.proximo
         return removido
+class Noh3:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+class PilhaEncadeada3:
+    def __init__(self):
+        self.cabeca = None
+
+    def push(self, valor):
+        novo_no = Noh3(valor)
+        novo_no.proximo = self.cabeca
+        self.cabeca = novo_no
+
+    def pop(self):
+        removido = self.cabeca.valor
+        self.cabeca = self.cabeca.proximo
+        return removido    
 
 
-pilha = PilhaEncadeada()
-pilha.push(2)
+
+'''
+
+rodando = True
+
+peçassegura = None
+
+while rodando:
+
+        print( " Escolha uma das opçoes:\n 0 - Regras da torre \n 1 - retirar uma peça de lugar\n 2 - Colocar uma peça em outro disco\n 3 - mostrar o disco que você está segurando\n 4 - Imprimir as torres\n 5 - Sair ")    
+        escolha = ()
+        escolha = int(input())
+    
+        print(escolha)
+        
+        print(rodando)
+
+
+        if escolha == 0:
+            print("Apenas um disco pode ser movido por vez e nunca um disco maior deve ficar por cima de um disco menor.")
+
+        elif escolha == 1:
+                escolhatorre = True
+                while escolhatorre:
+                    print("Escolha uma torre para remover uma peça!: \n Torre A \n Torre B \n Torre C  \n Torre D \n Voltar ao menu principal F ")
+                    mover = None
+                    mover = str(input()).upper()
+                    print(mover)
+                    if mover == "A":
+                        print("tente digitar o numero da torre!")
+
+                    elif mover == "B": 
+                        print("tente digitar o numero da torresfds!")
+                    elif mover == "C":   
+                        print("tente digitar o numero da torrfdse!")
+                    elif mover == "D":       
+                        print("tente digitar o numero da torrfdse!")
+                    elif mover == "F":
+                         break
+                    #else:
+                        #print("tente digitar o numero da torre!")    
+            
+        elif escolha == 2:
+                print("tente digitar o numero da torre!")
+
+
+        
+        elif escolha == 3:
+                print(f"Você está segurando a peça de tamanho {Peçasegura}")
+
+
+                if peçassegura == ():
+                    print("Você nao esta com nenhuma peça em mãos")
+            
+        elif escolha == 4:
+                print("tente digitar o numero da torre!")
+        
+        elif escolha == 5:
+            rodando == False
+
+'''    
+
+
+
+
+'''pilha.push(2)
 pilha.push(4)
 pilha.push(6)
 pilha.push(8)
-pilha.push(10)
+pilha.push(10)'''
 # fazer: tratar inserção na pilha cheia
 
 
 # fazer: chamar metodo que imprime a pilha
 
-print(pilha.pop())
-print(pilha.pop())
-print(pilha.pop())
-print(pilha.pop())
-print(pilha.pop())
-print(pilha.pop()) # fazer: tratar remoção de pilha vazia
-
+for i in range(1, 11):
+    pilha = PilhaEncadeada1()
+    pilha.push(i)
+print(f"pilha: {pilha}")
